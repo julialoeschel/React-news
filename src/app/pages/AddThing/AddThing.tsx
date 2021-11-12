a60beb261f84f260f216c794e7021ab2df74dbfd
Julia Loeschel <juloeschel@gmail.com>
2021-11-12 15:21:12 +0100

    Add Form and handleSubmit

--- src/app/pages/AddThing/AddThing.tsx
+++ src/app/pages/AddThing/AddThing.tsx
@@ -2,17 +2,22 @@ import React from 'react';
 import styled from 'styled-components';
 
 export default function AddThing(): JSX.Element {
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+  }
+
   return (
-    <StyleForm>
-      <label>
-        Name
+    <StyleForm action="submit" onSubmit={(event) => handleSubmit(event)}>
+      <StyledLabel>
+        Name <br />
         <input type="text" />
-      </label>
+      </StyledLabel>
       <label>
         Description
+        <br />
         <input type="text" />
       </label>
-      <button>Submitt Thing</button>
+      <button type="submit">Submett Thing</button>
     </StyleForm>
   );
 }
@@ -20,4 +25,12 @@ export default function AddThing(): JSX.Element {
 const StyleForm = styled.form`
   display: grid;
   margin-top: 20px;
+  gap: 20px;
+  border: solid 2px;
+  padding: 10px;
+  display: grid;
+`;
+
+const StyledLabel = styled.label`
+  display: block;
 `;
